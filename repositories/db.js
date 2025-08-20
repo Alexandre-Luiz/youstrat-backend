4270f60bc15d127ffd94f8980dd708912990d48f
Alexandre Elias <alexandre.alpe@gmail.com>
2025-08-20 01:03:18 -0300

    Fixing ipv4 connection string

--- repositories/db.js
+++ repositories/db.js
@@ -8,7 +8,7 @@ const dbPass = process.env.DB_PASS;
 const sequelize = new Sequelize(
   //`postgres://${dbUser}:${dbPass}@tuffi.db.elephantsql.com/${dbUser}`,
   // `postgresql://postgres:${dbPass}@db.${dbUser}.supabase.co:5432/postgres`,
-  `postgresql://postgres.${dbPass}:${dbUser}@aws-1-sa-east-1.pooler.supabase.com:5432/postgres`, //ipv4 pooler
+  `postgresql://postgres.${dbUser}:${dbPass}@aws-1-sa-east-1.pooler.supabase.com:5432/postgres`, //ipv4 pooler
 
   {
     dialect: 'postgres',
